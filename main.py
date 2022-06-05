e2ab126e4a58240af97a523aaf9fd2169de52c26
Innocent-Ogesiano <innocentekweogu@gmail.com>
2022-06-05 11:55:08 +0100

    rock-paper-scissors game

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ import random
 
 
 def rock_paper_scissors():
-    player1 = "COM"
-    player2 = input("Enter player name: ")
+    cpu = "CPU"
+    player = input("Enter player name: ")
     values = ["R", "P", "S"]
     while True:
-        player2_option = player2_game_option(values)
-        player1_option = cpu_random_option(values)
-        print(player2 + "(" + player2_option + ") : " + player1 + "(" + player1_option + ")")
+        player_option = player_game_option(values)
+        cpu_option = cpu_random_option(values)
+        print(player + "(" + player_option + ") : " + cpu + "(" + cpu_option + ")")
 
-        if player2_option == player1_option:
+        if player_option == cpu_option:
             print("It's a tie, play again")
-        elif (player1_option == "Rock" and player2_option == "Scissors") or \
-                (player1_option == "Paper" and player2_option == "Rock") or \
-                (player1_option == "Scissors" and player2_option == "Paper"):
+        elif (cpu_option == "Rock" and player_option == "Scissors") or \
+                (cpu_option == "Paper" and player_option == "Rock") or \
+                (cpu_option == "Scissors" and player_option == "Paper"):
 
-            print(player1 + " wins")
+            print(cpu + " wins")
             break
         else:
-            print(player2 + " wins")
+            print(player + " wins")
             break
 
 
-def player2_game_option(accepted_values):
+def player_game_option(accepted_values):
     option = input("pick an option between 'R', 'P' or 'S': ")
     while option not in accepted_values:
         print("Invalid option, try again!")
